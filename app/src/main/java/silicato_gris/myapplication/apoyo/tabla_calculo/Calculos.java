326612637d9fddbb38540af97546686440951860
chechis <jcalvarezg92@gmail.com>
2019-12-21 08:31:00 -0600

    calculos verificados

--- app/src/main/java/silicato_gris/myapplication/apoyo/tabla_calculo/Calculos.java
+++ app/src/main/java/silicato_gris/myapplication/apoyo/tabla_calculo/Calculos.java
@@ -2,13 +2,82 @@ package silicato_gris.myapplication.apoyo.tabla_calculo;
 
 public class Calculos {
 
-    
+
+    public Calculos() {
+    }
 
     public double relacionAC (double fc){
 
         double resistencia = 1.1497*(Math.exp(-0.0025*fc));
 
-        return resistencia;
+        return redondeo (resistencia, 3);
+    }
+    double sacoCemento = 42.2;
+
+    //proporcion unitaria
+    public double cantidadCemento (int cantidadAgua, double relAC){
+        double cemento = cantidadAgua/relAC;
+        return redondeo(cemento, 2);
+    }
+    public double cantidadAgregados (int cantidadAgua, double cantidadCemento, int pesoConcreto){
+        double agregados = pesoConcreto-cantidadAgua-cantidadCemento;
+        return redondeo(agregados, 2);
+    }
+    public double cantidadFino (double cantidadAgregados, double porcentaje){
+        double fino = cantidadAgregados*porcentaje;
+        return redondeo(fino, 2);
+    }
+    public double cantidadGrueso (double cantidadAgregados, double porcentaje){
+        double grueso = cantidadAgregados*(1-porcentaje);
+        return redondeo(grueso, 2);
+    }
+    //proporcion volumetrica
+    public double propArena (double cantidadCemento, double cantidadFino){
+        double arena = cantidadFino/cantidadCemento;
+        return redondeo(arena, 1);
+
+    }
+    public double propPiedrin (double cantidadCemento, double cantidadGrueso){
+        double grueso = cantidadGrueso/cantidadCemento;
+        return redondeo(grueso, 1);
+
+    }
+
+    //cantidad de material a comprar
+    public double comprarCemento(double cantidadCemento, double volumen){
+        double cemento = (cantidadCemento*volumen)/sacoCemento;
+        return Math.ceil(cemento);
+    }
+    public double comprarArena(double cantidadArena, double volumen, int pesoArenaCompactado){
+        double arena = (cantidadArena*volumen)/pesoArenaCompactado;
+        return redondeo(arena, 2);
+    }
+    public double comprarPiedrin(double cantidadPiedrin, double volumen, int pesoPiedrinCompactado){
+        double piedrin = (cantidadPiedrin*volumen)/pesoPiedrinCompactado;
+        return redondeo(piedrin, 2);
+    }
+    public double comprarAgua(double cantidadAgua, double volumen){
+        double agua = (cantidadAgua*volumen)/1000;
+        return redondeo(agua, 2);
+    }
+    //cantidad por saco
+    public double costalArena(int pesoArenaSuelto, double propVolArena){
+        double arena = (propVolArena * 1000*sacoCemento)/pesoArenaSuelto;
+        return redondeo(arena, 2);
+    }
+    public double costalPiedrin(int pesoPiedrinSuelto, double propVolPiedrin){
+        double arena = (propVolPiedrin * 1000*sacoCemento)/pesoPiedrinSuelto;
+        return redondeo(arena, 2);
+    }
+    public double costalAgua(double propVolAgua){
+        double arena = (propVolAgua * 1000*sacoCemento)/1000;
+        return redondeo(arena, 2);
+    }
+
+
+    public double redondeo (double numero, int numeroDecimales){
+        return Math.round(numero*Math.pow(10, numeroDecimales)) / Math.pow(10, numeroDecimales);
+
     }
 
 
--- app/src/main/java/silicato_gris/myapplication/fragmento/Ingreso.java
+++ app/src/main/java/silicato_gris/myapplication/fragmento/Ingreso.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import silicato_gris.myapplication.R;
@@ -27,11 +28,20 @@ import silicato_gris.myapplication.apoyo.tabla_calculo.Tablas;
 public class Ingreso extends Fragment {
 
     private TextInputLayout editResistencia, editPesoConcreto, editPesoFinoSuelto,
-            editPesoFinoComp, editPesoGruesoSuelto, editPesoGruesoCompac;
+            editPesoFinoComp, editPesoGruesoSuelto, editPesoGruesoCompac , editVolumen;
     private Spinner spnFactor, spnElemento, spnTMN;
 
     int tmn, asentamiento, factor;
 
+    private Calculos calculo = new Calculos();
+
+
+    private double relacionAC, propUnitariaCemento, propUnitariaAgregados, propUnitariaArena, propUnitariaPiedrin,
+            propUnitariaAgua, propVolArena, propVolPiedrin, comprarCemento, comprarArena, comprarPiedrin,
+            comprarAgua, costalArena, costalPiedrin, costalAgua;
+
+
+    TextView pruebaText;
 
     @Nullable
     @Override
@@ -44,12 +54,15 @@ public class Ingreso extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        pruebaText = (TextView) view.findViewById(R.id.prueba);
+
         editResistencia = (TextInputLayout) view.findViewById(R.id.edit_resistencia);
         editPesoConcreto = (TextInputLayout) view.findViewById(R.id.edit_peso_concreto);
         editPesoFinoSuelto = (TextInputLayout) view.findViewById(R.id.edit_peso_fino_suelto);
         editPesoFinoComp = (TextInputLayout) view.findViewById(R.id.edit_peso_fino_compactado);
         editPesoGruesoSuelto = (TextInputLayout) view.findViewById(R.id.edit_peso_grueso_suelto);
         editPesoGruesoCompac = (TextInputLayout) view.findViewById(R.id.edit_peso_grueso_compactado);
+        editVolumen = (TextInputLayout) view.findViewById(R.id.edit_volumen);
 
         spnFactor = (Spinner) view.findViewById(R.id.spinner_factor);
         spnElemento = (Spinner) view.findViewById(R.id.spinner_elemento);
@@ -107,11 +120,12 @@ public class Ingreso extends Fragment {
                 Calculos calculos = new Calculos();
 
                 Double ac = calculos.relacionAC(resistencia);
+                Double acRedon = calculos.redondeo(ac, 3);
 
                 int cantidadAgua = tablas.tablaAgua(asentamiento, tmn);
                 Double cantidadArena = tablas.tablaArena(tmn);
 
-                Toast.makeText(context, "Agua lt"+cantidadAgua+"  Cantidad de arena"+cantidadArena+" Rel AC"+ac+resistencia, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "Agua lt"+cantidadAgua+"  Cantidad de arena"+cantidadArena+" Rel AC"+acRedon, Toast.LENGTH_LONG).show();
 
                // listaTarea.add(new Tarea(id, nombreTarea, estudiante, asignatura, nota));
             }while (cursor.moveToNext());
@@ -127,12 +141,13 @@ public class Ingreso extends Fragment {
 
         boolean login = true;
         if (editResistencia !=null && editPesoConcreto != null){
-            double resistencia = Integer.parseInt(editResistencia.getEditText().getText().toString());
+            double resistencia = Double.parseDouble(editResistencia.getEditText().getText().toString());
             int pesoConcreto = Integer.parseInt(editPesoConcreto.getEditText().getText().toString());
             int pesoFinoSuelto = Integer.parseInt(editPesoFinoSuelto.getEditText().getText().toString());
             int pesoFinoCompac = Integer.parseInt(editPesoFinoComp.getEditText().getText().toString());
             int pesoGruesoSuelto = Integer.parseInt(editPesoGruesoSuelto.getEditText().getText().toString());
             int pesoGruesoCompac = Integer.parseInt(editPesoGruesoCompac.getEditText().getText().toString());
+            double volumen = Double.parseDouble(editVolumen.getEditText().getText().toString());
 
             switch (spnFactor.getSelectedItemPosition()){
                 case 0:
@@ -188,6 +203,33 @@ public class Ingreso extends Fragment {
 
             }
 
+            Tablas tablas = new Tablas(asentamiento, tmn);
+
+            double fc = resistencia+Double.parseDouble(String.valueOf(factor));
+            relacionAC = calculo.relacionAC(fc);
+
+            propUnitariaAgua = tablas.tablaAgua(asentamiento, tmn);
+            propUnitariaCemento = calculo.cantidadCemento(tablas.tablaAgua(asentamiento, tmn),relacionAC);
+            propUnitariaAgregados = calculo.cantidadAgregados(tablas.tablaAgua(asentamiento, tmn), propUnitariaCemento, pesoConcreto);
+            propUnitariaArena = calculo.cantidadFino(propUnitariaAgregados, tablas.tablaArena(tmn));
+            propUnitariaPiedrin = calculo.cantidadGrueso(propUnitariaAgregados, tablas.tablaArena(tmn));
+
+            propVolArena = calculo.propArena(propUnitariaCemento, propUnitariaArena);
+            propVolPiedrin = calculo.propPiedrin(propUnitariaCemento, propUnitariaPiedrin);
+
+            comprarCemento = calculo.comprarCemento(propUnitariaCemento, volumen);
+            comprarArena = calculo.comprarArena(propUnitariaArena, volumen, pesoFinoCompac);
+            comprarPiedrin = calculo.comprarPiedrin(propUnitariaPiedrin, volumen, pesoGruesoCompac);
+            comprarAgua = calculo.comprarAgua(propUnitariaAgua, volumen);
+
+            costalArena = calculo.costalArena(pesoFinoSuelto, propVolArena);
+            costalPiedrin = calculo.costalPiedrin(pesoGruesoSuelto, propVolPiedrin);
+            costalAgua = calculo.costalAgua(relacionAC);
+
+            pruebaText.setText(relacionAC+"\n"+propUnitariaAgua+"\n"+propUnitariaCemento+"\n"+propUnitariaAgregados+"\n"
+                    +propUnitariaArena+"\n"+propUnitariaPiedrin+"\n"+propVolArena+"\n"+propVolPiedrin+"\n"+comprarCemento+"\n"+comprarArena+"\n"
+                    +comprarPiedrin+"\n"+comprarAgua+"\n"+costalArena+"\n"+costalPiedrin+"\n"+costalAgua+"\n");
+
             BaseDatos baseDatos = new BaseDatos(getContext());
 
             SQLiteDatabase sq = baseDatos.getWritableDatabase();
@@ -198,7 +240,6 @@ public class Ingreso extends Fragment {
 
     }
 
-  //colocar un metodo para hacer calculos y ponerlo arriba
 
     @Override
     public void onResume() {
