1c9dd6fb3b810e6899619ac0a9fd356cc8702bb2
chechis <jcalvarezg92@gmail.com>
2019-12-30 22:11:59 -0600

    fin

--- app/src/main/java/silicato_gris/myapplication/Main2Activity.java
+++ app/src/main/java/silicato_gris/myapplication/Main2Activity.java
@@ -13,11 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
 
 
 import java.util.ArrayList;
@@ -42,6 +47,9 @@ public class Main2Activity extends AppCompatActivity implements AlertaIngreso.In
     RecyclerView recyclerView;
     private  List<Concreto> listaConcreto = new ArrayList<>();
     private  List<ConcretoEditar> listaEditar = new ArrayList<>();
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    AdRequest adRequest;
 
 
     @Override
@@ -64,8 +72,81 @@ public class Main2Activity extends AppCompatActivity implements AlertaIngreso.In
             }
         });
 
-        MobileAds.initialize(this);
+        //banner
+        MobileAds.initialize(this, getString(R.string.id_anuncios));
+        mAdView = findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
+        mAdView.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+        //interstitial
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.intersticial));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the interstitial ad is closed.
+            }
+        });
     }
 
     private void llenandoAdapter (List<Concreto> lista){
@@ -200,6 +281,12 @@ public class Main2Activity extends AppCompatActivity implements AlertaIngreso.In
         actualizarLista();
         actualizarEditar();
         adapterProporcion.notifyDataSetChanged();
+        if (mInterstitialAd.isLoaded()&& mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            cargarAnuncio();
+
+        }
     }
 
     @Override
@@ -327,5 +414,17 @@ public class Main2Activity extends AppCompatActivity implements AlertaIngreso.In
         actualizarEditar();
         adapterProporcion.notifyDataSetChanged();
 
+        if (mInterstitialAd.isLoaded()&& mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            cargarAnuncio();
+
+        }
+
+    }
+    private void cargarAnuncio(){
+        adRequest = new AdRequest.Builder().build();
+        mInterstitialAd.loadAd(adRequest);
+        mInterstitialAd.show();
     }
 }
